4d50b71e63616da5412e622826958d65f5b828b5
camillafalk <ccamilla.falk@gmail.com>
2025-11-11 14:01:20 +0100

    Started getting todays date and checkInDate compared.

--- functions/DeleteBookings/index.js
+++ functions/DeleteBookings/index.js
@@ -1,33 +1,53 @@
-const { sendResponse } =require('../../response/index.js');
-const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
-const { DeleteCommand, DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
+const { sendResponse } = require("../../response/index.js");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  DeleteCommand,
+  DynamoDBDocumentClient,
+} = require("@aws-sdk/lib-dynamodb");
 
 const client = new DynamoDBClient({});
 const db = DynamoDBDocumentClient.from(client);
 
 exports.handler = async (event) => {
+  const { id } = event.pathParameters;
 
-    const { id } = event.pathParameters;
+  const command = new DeleteCommand({
+    TableName: "hotell-db-v2",
+    Key: { id: id },
+    ReturnValues: "ALL_OLD",
+  });
 
-    const command = new DeleteCommand({
-        TableName: 'hotell-db-v2',
-        Key: { id: id },
-        ReturnValues: 'ALL_OLD'
-    });
+  try {
+    const deleting = await db.send(command);
 
-    try {
-
-       const deleting = await db.send(command)
-
-        if(!deleting.Attributes) {
-            return sendResponse(404, { success : false, message : 'No booking found to delete'})
-        }
-
-        return sendResponse(200, { success : true, message : 'Booking was successfully deleted!', deletedBooking: deleting.Attributes })
-
-    } catch(error) {
-
-        return sendResponse(500, { success : false, message: 'could not delete booking', error: error.message});
-
+    if (!deleting.Attributes) {
+      return sendResponse(404, {
+        success: false,
+        message: "No booking found to delete",
+      });
     }
-} 
+
+    const todaysDate = new Date().toISOString().split("T")[0];
+    const checkInDate = deleting.Attributes.checkInDate;
+
+    if (checkInDate == todaysDate) {
+      return sendResponse(500, {
+        success: false,
+        message: "You can't delete booking so soon to check in.",
+        error: error.message,
+      });
+    }
+
+    return sendResponse(200, {
+      success: true,
+      message: "Booking was successfully deleted!",
+      deletedBooking: deleting.Attributes,
+    });
+  } catch (error) {
+    return sendResponse(500, {
+      success: false,
+      message: "could not delete booking",
+      error: error.message,
+    });
+  }
+};
